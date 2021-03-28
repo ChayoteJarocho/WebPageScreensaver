c1642fac59c9dd1ce6a7d57485949b0af2749fd8
ChayoteJarocho <77412126+ChayoteJarocho@users.noreply.github.com>
2021-03-28 13:03:06 -0700

    Address info Roslyn messages

--- Preferences.cs
+++ Preferences.cs
@@ -58,14 +58,14 @@ namespace WebPageScreensaver
         {
             get
             {
-                Dictionary<int, ScreenInformation> screens = new Dictionary<int, ScreenInformation>();
+                var screens = new Dictionary<int, ScreenInformation>();
 
                 MultiScreenMode multiScreenMode = MultiScreen;
                 for (int screenNumber = 0; screenNumber < ScreenCount; screenNumber++)
                 {
                     string screenKeyName = $"Display{screenNumber + 1}"; // To match tab name
                     RegistryKey subKey = RootKey.GetOrCreateSubKey(screenKeyName);
-                    ScreenInformation info = new ScreenInformation(subKey, screenNumber, multiScreenMode);
+                    var info = new ScreenInformation(subKey, screenNumber, multiScreenMode);
                     screens.Add(screenNumber, info);
                 }
 
