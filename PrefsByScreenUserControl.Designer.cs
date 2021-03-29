33fc44ada6a62bc2ecf7bcccd6c25d9b881ae689
ChayoteJarocho <77412126+ChayoteJarocho@users.noreply.github.com>
2021-03-28 17:17:51 -0700

    unnecessary parenthesis

--- PrefsByScreenUserControl.Designer.cs
+++ PrefsByScreenUserControl.Designer.cs
@@ -13,7 +13,7 @@
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (_components != null))
+            if (disposing && _components != null)
             {
                 _components.Dispose();
             }
